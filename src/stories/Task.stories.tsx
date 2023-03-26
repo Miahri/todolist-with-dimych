f4eef1234cf5eaa499890691c2e6efd866e8b06a
Miahri <mahrihommadova@gmail.com>
2023-03-26 13:58:16 +0300

    added stories for Task

---
+++ src/stories/Task.stories.tsx
@@ -0,0 +1,26 @@
+import {action} from '@storybook/addon-actions'
+import {Task} from "../Task";
+
+export default {
+    title: 'Task',
+    component: Task
+}
+
+const changeStatusCallback = action("Status was changed");
+const onChangeTaskTitleCallback = action("Title was changed");
+const removeTaskCallback = action("Task was removed");
+
+export const TaskExample = (props: any) => {
+    return <>
+        <Task todolistId={"todolistId1"}
+                 task={{id: '1', title: "JS", isDone: false}}
+                 changeStatus={changeStatusCallback}
+                 onChangeTaskTitle={onChangeTaskTitleCallback}
+                 removeTask={removeTaskCallback}/>
+        <Task todolistId={"todolistId2"}
+              task={{id: '2', title: "CSS", isDone: true}}
+              changeStatus={changeStatusCallback}
+              onChangeTaskTitle={onChangeTaskTitleCallback}
+              removeTask={removeTaskCallback}/>
+    </>
+}
