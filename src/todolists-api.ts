155696e526df3adcad7215e74205d8b8699b5432
Miahri <mahrihommadova@gmail.com>
2023-04-12 17:02:16 +0300

    added enum for status and priority

--- src/todolists-api.ts
+++ src/todolists-api.ts
@@ -12,6 +12,21 @@ const instance = axios.create({
     ...settings
 })
 
+export enum TaskStatuses {
+    New = 0,
+    InProgress = 1,
+    Completed = 2,
+    Draft = 3
+}
+
+export enum TaskPriorities {
+    Low = 0,
+    Middle = 1,
+    High = 2,
+    Urgently = 3,
+    Later = 4
+}
+
 export type TodolistType = {
     id: string
     title: string
@@ -23,8 +38,8 @@ export type TaskType = {
     description: string
     title: string
     completed: boolean
-    status: number
-    priority: number
+    status: TaskStatuses
+    priority: TaskPriorities
     startDate: string
     deadline: string
     id: string
@@ -68,7 +83,6 @@ type GetTasksResponseType = {
 type UpdateTaskType = {
     title: string
     description: string
-    completed: boolean
     status: number
     priority: number
     startDate: string
