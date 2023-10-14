3186b9f8c1eecc11185d599508fba51a76a126be
Miahri <mahrihommadova@gmail.com>
2023-10-14 15:01:16 +0300

    added thunkTryCatch to app-reducer

--- src/features/Application/application-reducer.ts
+++ src/features/Application/application-reducer.ts
@@ -1,57 +1,56 @@
-import {authAPI} from "api/todolists-api"
-import {createSlice} from '@reduxjs/toolkit'
-import {appActions} from '../CommonActions/App'
+import { authAPI } from "api/todolists-api";
+import { createSlice } from "@reduxjs/toolkit";
+import { appActions } from "../CommonActions/App";
 import { createAppAsyncThunk } from "utils/createAppAsyncThunk";
+import { thunkTryCatch } from "utils/thunkTryCatch";
+import { handleAsyncServerAppError } from "utils/error-utils";
 
-const initializeApp = createAppAsyncThunk<{isLoggedIn: boolean}, undefined>('application/initializeApp',
-  async (_, thunkAPI) => {
-    const {dispatch, rejectWithValue} = thunkAPI;
-    try{
-        dispatch(appActions.setAppStatus({status: 'loading'}));
-        const res = await authAPI.me()
-        if (res.data.resultCode === 0) {
-            return {isLoggedIn: true};
-        } else {
-            return rejectWithValue(null);
-        }
-    } catch (error: any) {
-        return rejectWithValue(null);
+const initializeApp = createAppAsyncThunk<{ isLoggedIn: boolean }, undefined>
+("application/initializeApp", async (_, thunkAPI) => {
+  const { dispatch, rejectWithValue } = thunkAPI;
+  return thunkTryCatch(thunkAPI, async () => {
+    const res = await authAPI.me();
+    if (res.data.resultCode === 0) {
+      return { isLoggedIn: true };
+    } else {
+      handleAsyncServerAppError(res.data, dispatch);
+      return rejectWithValue(null);
     }
-
-})
+  });
+});
 
 export const asyncActions = {
-    initializeApp
-}
+  initializeApp
+};
 
 export const slice = createSlice({
-    name: 'app',
-    initialState: {
-        status: 'idle',
-        error: null,
-        isInitialized: false
-    } as InitialStateType,
-    reducers: {},
-    extraReducers: builder => {
-        builder
-            .addCase(initializeApp.fulfilled, (state, action) => {
-                state.isInitialized = true
-            })
-            .addCase(appActions.setAppStatus, (state, action) => {
-                state.status = action.payload.status
-            })
-            .addCase(appActions.setAppError, (state, action) => {
-                state.error = action.payload.error
-            })
-    }
-})
+  name: "app",
+  initialState: {
+    status: "idle",
+    error: null,
+    isInitialized: false
+  } as InitialStateType,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(initializeApp.fulfilled, (state, _) => {
+        state.isInitialized = true;
+      })
+      .addCase(appActions.setAppStatus, (state, action) => {
+        state.status = action.payload.status;
+      })
+      .addCase(appActions.setAppError, (state, action) => {
+        state.error = action.payload.error;
+      });
+  }
+});
 
-export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+export type RequestStatusType = "idle" | "loading" | "succeeded" | "failed"
 export type InitialStateType = {
-    // происходит ли сейчас взаимодействие с сервером
-    status: RequestStatusType
-    // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
-    error: string | null
-    // true когда приложение проинициализировалось (проверили юзера, настройки получили и т.д.)
-    isInitialized: boolean
+  // происходит ли сейчас взаимодействие с сервером
+  status: RequestStatusType
+  // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
+  error: string | null
+  // true когда приложение проинициализировалось (проверили юзера, настройки получили и т.д.)
+  isInitialized: boolean
 }
