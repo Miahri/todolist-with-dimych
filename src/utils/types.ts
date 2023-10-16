6d1675f459e9c673e9c193495384203a7727325e
Miahri <mahrihommadova@gmail.com>
2023-10-16 16:09:16 +0300

    refatoring

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,9 +1,5 @@
-import {rootReducer, store} from 'app/store'
-import {FieldErrorType} from 'api/types'
+import {store} from 'app/store'
 
-// redux common types
-export type RootReducerType = typeof rootReducer
 // определить автоматически тип всего объекта состояния
-export type AppRootStateType = ReturnType<RootReducerType>
+export type AppRootStateType = ReturnType<typeof store.getState>
 export type AppDispatchType = typeof store.dispatch
-export type ThunkError = { rejectValue: { errors: Array<string>, fieldsErrors?: Array<FieldErrorType> } }
